38ae324b9f93b16571ff8934d09227b815298c66
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-06-24 15:26:02 +0100

    fix indentation

--- lop.py
+++ lop.py
@@ -14,13 +14,13 @@ def synthetic_LOP(n, m, phi):
   return instance
 
 def get_fitness(perm, instance):
-# In case it is not numpy array
-    perm = np.asarray(perm)
-    n = len(perm) #sum in the upper triangle. we have to maximize this
-    sol = 0
-    for i in range(n):
-        sol += instance[perm[i], perm[i:n]]
-    return sol
+  # In case it is not numpy array
+  perm = np.asarray(perm)
+  n = len(perm) #sum in the upper triangle. we have to maximize this
+  sol = 0
+  for i in range(n):
+    sol += instance[perm[i], perm[i:n]]
+  return sol
 
 # Linear Ordering Problem
 class LOP:
