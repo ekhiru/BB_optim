f7989b08fc53ec9d5eddfb935d0ba609affd7f38
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-01-11 18:12:24 +0100

    cegores csv

---
+++ collect-data.R
@@ -0,0 +1,9 @@
+cegores <- NULL
+for(i in 1:30) {
+  cegores <- rbind(cegores,
+                   readRDS(paste0("cegores-er0-r", i, ".rds")),
+                   readRDS(paste0("cegores-er1-r", i, ".rds")))
+}
+saveRDS(cegores, file="cegores.rds")
+write.csv(cegores, file="cegores.csv", row.names=FALSE)
+aggregate(cegores$time, list(er=cegores$eval_ranks), mean)
