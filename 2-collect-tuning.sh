e1d44804e47ae7164507e967f3819a28d2297c96
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-01-24 12:51:58 +0000

    new tuning results

--- 2-collect-tuning.sh
+++ 2-collect-tuning.sh
@@ -28,23 +28,24 @@ gen_lop_synthetic() {
 
 ## For QAP, PFSP instances
 INSTANCES="\
-  qap/kra30a.dat \
-  qap/kra30b.dat \
- qap/nug30.dat \
- qap/tho30.dat \
- pfsp/rec13.txt \
+qap/kra30a.dat \
+qap/kra30b.dat \
+qap/nug30.dat \
+qap/tho30.dat \
+pfsp/rec13.txt \
 pfsp/rec19.txt \
+lop/RandB/N-p40-02 \
+lop/IO/N-t59d11xx \
+lop/SGB/N-sgb75.02 \
+lop/xLOLIB/N-be75eec_150 \
 "
-
-###### For LOLIB instances
-#INSTANCES="$(grep -v '#' lolib-instances.txt | tr '\n' ' ')"
 
 ###### Synthetic LOP instances
 INSTANCES=$(gen_lop_synthetic $INSTANCES)
 
-OUTFILE="$OUTDIR/tuning.csv"
+OUTFILE="$BINDIR/results-er0/tuning.csv"
 echo "instance,seed,umm_m_ini,budgetMM,r_1,r_2,budget,eval_ranks,fitness" > $OUTFILE 
-counter=0
+
 for instance in $INSTANCES; do
     RESULTS="$OUTDIR/tuning-er?/$instance"
     echo "Reading $RESULTS"
