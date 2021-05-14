6bc4a53a135f19ddd960c71d23414531395511ed
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-05-14 18:53:24 +0100

    4-collect_results.py: Improve it.

--- 4-collect_results.py
+++ 4-collect_results.py
@@ -3,8 +3,9 @@ import os
 import os.path
 from glob import glob
 import re
-import pandas as pd
 import sys
+import numpy as np
+import pandas as pd
 
 if len(sys.argv) < 2:
     res_dir = "./results/"
@@ -21,21 +22,23 @@ for root, dirs, files in os.walk(res_dir):
     unique_files = []
     for file in files:
         result = re.match(r"(.+)-r([0-9]+)\.csv\.xz", file)
-        if result:
+        if result and result.group(1):
             unique_files.append(result.group(1))
     unique_files = set(unique_files)
     for file in unique_files:
         runs = []
         run_data = []
+        to_delete = []
         for run_file in sorted(glob(root + os.sep + file + "-r*.csv.xz")):
-            result = re.match(r".+-r([0-9]+)\.csv\.xz", run_file)
+            result = re.search(rf"{re.escape(os.sep)}{re.escape(file)}-r([0-9]+)\.csv\.xz$", run_file)
+            if not result: continue
             runs.append(result.group(1))
             try:
                 run_data.append(pd.read_csv(run_file))
+                to_delete.append(run_file)
             except:
                 print(f"Error: reading {run_file}")
                 raise
-
         res_file = root + os.sep + file + ".csv.xz"
         if os.path.isfile(res_file):
             df = pd.read_csv(res_file)
@@ -44,9 +47,9 @@ for root, dirs, files in os.walk(res_dir):
             print(f'Replacing {len(common_runs)} runs {common_runs} ')
             df = df[~df["seed"].isin(runs)]
             run_data.insert(0, df)
-        df = pd.concat(run_data, ignore_index = True)
+        df = pd.concat(run_data, ignore_index = True, sort = False)
         print(f"Writing runs {runs} in {res_file}")
         df.to_csv(res_file, index=False, compression = "xz")
-        for run_file in glob(root + os.sep + file + "-r*.csv.xz"):
+        for run_file in to_delete:
             print(f"Deleting {run_file}")
             os.remove(run_file)
